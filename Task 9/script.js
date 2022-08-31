0485f03ed352fd43235ac1e6efb8e254928a32ba
Alvita Jurkute <jurkute.alvita@gmail.com>
2022-08-31 18:48:59 +0300

    Padaryta 9 užduotis

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,19 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+function Movie(title, director, budget) {
+  (this.title = title),
+    (this.director = director),
+    (this.budget = budget),
+    (this.wasExpensive = function () {
+      if (this.budget > 100000000) {
+        return true;
+      } else if (this.budget < 100000000) {
+        return false;
+      }
+    });
+}
+
+const titanikas = new Movie("Titanic", "James Cameron", 200000000);
+titanikas.wasExpensive();
